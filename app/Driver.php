769c9f72116ddaa39c051f3dfe9aada52978c935
mhamayda7 <m.hamayda7@gmail.com>
2021-09-08 00:28:31 +0300

    1.14

--- app/Driver.php
+++ app/Driver.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Driver extends Model
 {
     protected $fillable = [
-        'id', 'first_name','last_name','phone_number','country_id','phone_with_code','email','gender','password','date_of_birth','licence_number','fcm_token','currency','overall_ratings', 'no_of_ratings', 'daily', 'rental', 'outstation'
+        'id', 'full_name','phone_number','country_id','phone_with_code','email','gender','password','date_of_birth','licence_number','fcm_token','currency','overall_ratings', 'no_of_ratings', 'daily', 'rental', 'outstation'
     ];
 }
