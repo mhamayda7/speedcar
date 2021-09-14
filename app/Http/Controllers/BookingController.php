65372ad5f333dbf1b259ffe6b87a837c932d0511
mhamayda7 <m.hamayda7@gmail.com>
2021-09-14 11:59:09 +0300

    1.119

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -977,7 +977,7 @@ class BookingController extends Controller
         // $trip_request = TripRequest::findOrFail($id);
         // $status = $trip_request->status;
         elseif ($trip_request->status == 3) {
-            $trip = Trip::select('trip_id', 'customer_id', 'driver_id', 'vehicle_id', 'status', 'pickup_address', 'drop_address', 'patment_method')->where('customer_id', $id)->get()->last();
+            $trip = Trip::select('trip_id', 'customer_id', 'driver_id', 'vehicle_id', 'status', 'pickup_address', 'drop_address', 'payment_method')->where('customer_id', $id)->get()->last();
             $driver = Driver::select('full_name', 'profile_picture','overall_ratings')->where('id', $trip->driver_id)->get()->last();
             $vehicle = DriverVehicle::select('vehicle_image', 'brand', 'vehicle_name', 'vehicle_number')->where('driver_id', $trip->driver_id)->get();
             return response()->json([
