0b906d32bd7eb86cc1d44a5a66790e1ea04a9ab5
mhamayda7 <m.hamayda7@gmail.com>
2021-09-16 12:44:23 +0300

    1

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -918,7 +918,7 @@ class BookingController extends Controller
             $trans = [];
             //invoice
             $payment_method = Trip::where('trip_id', $input['trip_id'])->get(['payment_method']);
-            dd($input['trip_id']);
+            dd($payment_method);
             switch ($payment_method)
             {
 
