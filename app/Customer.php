c552895e91ba3b5d7e59a26aba5c9345194dbde6
mhamayda7 <m.hamayda7@gmail.com>
2021-09-24 13:45:37 +0300

    aa

--- app/Customer.php
+++ app/Customer.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
 
 class Customer extends Model
 {
+    use HasFactory, HasApiTokens;
     protected $fillable = [
         'id', 'country_id', 'full_name', 'phone_number','email','password','profile_picture','status', 'country_code','currency_short_code', 'phone_with_code', 'fcm_token', 'currency', 'wallet','gender', 'otp'
     ];
