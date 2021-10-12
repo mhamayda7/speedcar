d7a7ac5dd4e484e79f9f1b66a06a962f1525cc80
mhamayda7 <m.hamayda7@gmail.com>
2021-10-13 00:31:41 +0300

    s

--- app/NotificationMessage.php
+++ app/NotificationMessage.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class NotificationMessage extends Model
 {
      protected $fillable = [
-        'id','type','message','image','status','created_at','updated_at'
+        'id','user_id','country_id','type','title','message','image','status','created_at','updated_at'
     ];
 }
