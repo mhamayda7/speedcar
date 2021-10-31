215de4a92295d2c29431ef45b16510a449ae540e
Mahamoud Alhamayda <75016622+mhamayda7@users.noreply.github.com>
2021-10-31 18:48:06 +0400

    ass

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -1116,7 +1116,7 @@ class BookingController extends Controller
             // $price_per_km = number_format((float)$vehicle->price_per_km, 2, '.', '');
             // $price_time = number_format((float)$vehicle->price_time, 2, '.', '');
             $interval = (strtotime($trip->end_time) - strtotime($trip->start_time)) / 60;
-            $price_time = $interval * $vehicle->price_time;
+            $price_time = 10;
             $price_distance = $distance * $vehicle->price_per_km;
 
             $fare = $vehicle->base_fare + $price_time + $price_distance ;
