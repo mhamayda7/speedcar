6871ee69b2318e5461fb5c1b960e34c49e4df58e
Mahamoud Alhamayda <75016622+mhamayda7@users.noreply.github.com>
2021-11-01 23:45:00 +0400

    aa

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -1123,7 +1123,7 @@ class BookingController extends Controller
 
             $totalSecondsDiff = abs($end-$start);
             $time = $totalSecondsDiff / 60 ;
-            dd($time);
+            dd($start);
             $price_time = $time * $vehicle->price_time;
             $price_time = number_format((float)$price_time, 2, '.', '');
             $price_distance = $distance * $vehicle->price_per_km;
