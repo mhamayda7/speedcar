7ae033ae00c6eba876f7a7f06696f01de677b20f
Mahamoud Alhamayda <m.hamayda7@gmail.com>
2021-12-26 21:01:51 +0400

    1.19

--- app/PromoCode.php
+++ app/PromoCode.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class PromoCode extends Model
 {
      protected $fillable = [
-        'id','group_customer','promo_code','promo_type','description','discount','count_used','status','valid_to','created_at','updated_at'
+        'id','group_customer','promo_code','promo_type','description','title','discount','count_used','status','valid_to','created_at','updated_at'
     ];
 }
