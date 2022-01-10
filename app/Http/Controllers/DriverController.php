edd80e488dafbffb87a2152a1250ed02c7a5b071
Mahamoud Alhamayda <75016622+mhamayda7@users.noreply.github.com>
2022-01-10 23:35:52 +0400

    1

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -978,7 +978,7 @@ class DriverController extends Controller
             $price_time = number_format((float)$vehicle->price_time, 2, '.', '');
             $interval = (strtotime($trip->end_time) - strtotime($trip->start_time)) / 60;
             $trip['arrive_driver'] = $trip['updated_at']->format('Y-m-d H:i');
-
+            dd($trip['arrive_driver']);
             $trip['cancel_time'] = (strtotime($trip['updated_at']) + 300)->format('Y-m-d H:i');
             // $fare = number_format((float)$base_far + ($price_per_km * $distance) + ($price_time * $interval));
 
