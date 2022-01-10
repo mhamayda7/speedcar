ca47ac4cc5f66139dc294691ff8595dd99c067d3
Mahamoud Alhamayda <75016622+mhamayda7@users.noreply.github.com>
2022-01-10 20:54:40 +0400

    1111

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -263,8 +263,8 @@ class BookingController extends Controller
         $min_distance = 0;
         $min_driver_id = 0;
         $booking_searching_radius = TripSetting::value('booking_searching_radius');
-        foreach ($drivers as $key => $value) {
-            $amount = Driver::where('id', 219)->value('wallet');
+        foreach ($drivers as  $value) {
+            $amount = Driver::where('id', $value['driver_id'])->value('wallet');
             if ($amount > (-1)) {
                 $distance = $this->distance($input['pickup_lat'], $input['pickup_lng'], $value['lat'], $value['lng'], 'K');
                 if ($value['online_status'] == 1 && $value['booking_status'] == 0) {
