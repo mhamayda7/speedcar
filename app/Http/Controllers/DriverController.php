437b11df6fbe84ef6828bda7ab7cceabcc17e43e
Mahamoud Alhamayda <75016622+mhamayda7@users.noreply.github.com>
2022-01-19 16:18:29 +0400

    11

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -967,8 +967,9 @@ class DriverController extends Controller
                 "status" => 0
             ]);
         } else {
-            $customer = Customer::where('id', $trip->customer_id)->select('full_name', 'phone_with_code', 'profile_picture', 'rating')->get();
-
+            $customer = Customer::where('id', $trip->customer_id)->select('full_name', 'phone_with_code', 'profile_picture', 'rating')->first();
+            // dd($customer['rating']);
+            $customer['rating'] = number_format((float)$customer['rating'], 2, '.', '');
             $trip['price_wait'] = DailyFareManagement::where('id', 1)->value('price_wait');
             $inovice = array();
             $trip->start_time;
