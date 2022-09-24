856823a4bcaa769644963586e12f589639b35307
Mahamoud Alhamayda <m.hamayda7@gmail.com>
2022-09-24 16:48:10 +0400

    1

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -254,7 +254,7 @@ class BookingController extends Controller
         $booking_searching_radius = TripSetting::value('booking_searching_radius');
 
         foreach ($drivers as  $value) {
-            // dd($value);
+            dd($value);
             $amount = Driver::where('id', $value['driver_id'])->value('wallet');
             if ($amount > (-1)) {
                 $distance = $this->distance($input['pickup_lat'], $input['pickup_lng'], $value['lat'], $value['lng'], 'K');
