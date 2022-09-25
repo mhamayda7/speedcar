be8b78f10eb9df4ad5950de9f6fa2b28fe10d5d1
Mahamoud Alhamayda <m.hamayda7@gmail.com>
2022-09-25 22:25:56 +0400

    2

--- app/Http/Controllers/changeDriverController.php
+++ app/Http/Controllers/changeDriverController.php
@@ -25,7 +25,7 @@ class changeDriverController extends Controller
             foreach ($tripRequests as $key => $tripRequest) {
                 // $trip_request = TripRequest::where('id', $tripRequest['request_id'])->first();
                 // $driver = $database->getReference('/drivers/'.$tripRequest['driver_id'])->getSnapshot()->getValue();
-                $this->newDriver($tripRequest['request_id'], $tripRequest['driver_id']);
+                $this->getrequest($tripRequest['request_id'], $tripRequest['driver_id']);
                 // dd($trip_request->pickup_lat. $trip_request->pickup_lng. $driver['lat']. $driver['lng']);
                 // $distance = $this->distance($trip_request->pickup_lat, $trip_request->pickup_lng, $driver['lat'], $driver['lng'], 'K');
                 // dd($distance);
@@ -129,103 +129,102 @@ class changeDriverController extends Controller
     // }
 
 
-    // public function getrequest($trip_id, $driver_id)
-    // {
-    //     $trip = TripRequest::where('id', $trip_id)->first();
+    public function getrequest($trip_id, $driver_id)
+    {
+        $trip = TripRequest::where('id', $trip_id)->first();
 
-    //     $factory = (new Factory())->withDatabaseUri(env('FIREBASE_DB'));
-    //     $database = $factory->createDatabase();
+        $factory = (new Factory())->withDatabaseUri(env('FIREBASE_DB'));
+        $database = $factory->createDatabase();
 
-    //     $newPost = $database
-    //         ->getReference('/drivers/' . $driver_id)
-    //         ->update([
-    //             'booking_status' => 0
-    //         ]);
+        $newPost = $database
+            ->getReference('/drivers/' . $driver_id)
+            ->update([
+                'booking_status' => 0
+            ]);
 
-    //     if ($trip->booking_type == 1) {
-    //         TripRequest::where('id', $trip_id)->update(['status' => 4]);
-    //     }
+        if ($trip->booking_type == 1) {
+            TripRequest::where('id', $trip_id)->update(['status' => 4]);
+        }
 
-    //     $newcaptin = $this->find_car($trip_id);
+        $newcaptin = $this->find_car($trip_id);
 
-    //     $data['driver_id'] = $driver_id;
-    //     $data['trip_request_id'] = $trip_id;
-    //     $data['status'] = 0;
+        $data['driver_id'] = $driver_id;
+        $data['trip_request_id'] = $trip_id;
+        $data['status'] = 0;
 
-    //     DriverTripRequest::create($data);
-    //     // return response()->json([
-    //     //     "message" => 'Success',
-    //     //     "status" => 1
-    //     // ]);
-    // }
+        DriverTripRequest::create($data);
+        // return response()->json([
+        //     "message" => 'Success',
+        //     "status" => 1
+        // ]);
+    }
 
-    // public function find_car($trip_request_id)
-    // {
+    public function find_car($trip_request_id)
+    {
 
-    //     $factory = (new Factory())->withDatabaseUri(env('FIREBASE_DB'));
-    //     $database = $factory->createDatabase();
+        $factory = (new Factory())->withDatabaseUri(env('FIREBASE_DB'));
+        $database = $factory->createDatabase();
 
-    //     $trip_request = TripRequest::where('id', $trip_request_id)->first();
-    //     $drivers = $database->getReference('/drivers/')->getSnapshot()->getValue();
-    //     $rejected_drivers = DriverTripRequest::where('trip_request_id', $trip_request_id)->where('status', 0)->pluck('driver_id')->toArray();
+        $trip_request = TripRequest::where('id', $trip_request_id)->first();
+        $drivers = $database->getReference('/drivers/')->getSnapshot()->getValue();
+        $rejected_drivers = DriverTripRequest::where('trip_request_id', $trip_request_id)->where('status', 0)->pluck('driver_id')->toArray();
 
-    //     $min_distance = 0;
-    //     $min_driver_id = 0;
-    //     $oldDriver = $database->getReference('/triprequest/' . $trip_request_id)->getSnapshot()->getValue()['driver_id'];
+        $min_distance = 0;
+        $min_driver_id = 0;
+        $oldDriver = $database->getReference('/triprequest/' . $trip_request_id)->getSnapshot()->getValue()['driver_id'];
 
-    //     foreach ($drivers as $key => $value) {
-    //         if (!in_array($value['driver_id'], $rejected_drivers)) {
-    //             if ($value['online_status'] == 1 && $value['booking_status'] == 0) {
-    //                 $amount = Driver::where('id', $value['driver_id'])->value('wallet');
-    //                 if ($amount > (-1)) {
-    //                     $distance = $this->distance($trip_request->pickup_lat, $trip_request->pickup_lng, $value['lat'], $value['lng'], 'K');
-    //                     if ($min_distance == 0) {
-    //                         $min_distance = $distance;
-    //                         $min_driver_id = $value['driver_id'];
-    //                     } else if ($distance < $min_distance) {
-    //                         $min_distance = $distance;
-    //                         $min_driver_id = $value['driver_id'];
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     dd($min_driver_id);
-    //     if ($min_driver_id == 0 || $min_driver_id  == $oldDriver) {
+        foreach ($drivers as $key => $value) {
+            if (!in_array($value['driver_id'], $rejected_drivers)) {
+                if ($value['online_status'] == 1 && $value['booking_status'] == 0) {
+                    $amount = Driver::where('id', $value['driver_id'])->value('wallet');
+                    if ($amount > (-1)) {
+                        $distance = $this->distance($trip_request->pickup_lat, $trip_request->pickup_lng, $value['lat'], $value['lng'], 'K');
+                        if ($min_distance == 0) {
+                            $min_distance = $distance;
+                            $min_driver_id = $value['driver_id'];
+                        } else if ($distance < $min_distance) {
+                            $min_distance = $distance;
+                            $min_driver_id = $value['driver_id'];
+                        }
+                    }
+                }
+            }
+        }
+        if ($min_driver_id == 0 || $min_driver_id  == $oldDriver) {
 
-    //         $trip_request->update(['status' => 5]);
-    //         $custmoer_fcm = Customer::where('id', $trip_request->customer_id)->value('fcm_token');
-    //         try {
-    //             $this->send_fcm('نأسف جميع الكباتن مشغولين', 'جميع الكباتن مشغولين في رحلات أخرى', $custmoer_fcm);
-    //         } catch (Exception $e) {
-    //         }
-    //         $newPost = $database
-    //             ->getReference('/customers/' . $trip_request->customer_id)
-    //             ->update([
-    //                 'booking_id' => 0,
-    //                 'booking_status' => 0
-    //             ]);
+            TripRequest::where('id',$trip_request->id)->update(['status' => 5]);
+            $custmoer_fcm = Customer::where('id', $trip_request->customer_id)->value('fcm_token');
+            try {
+                $this->send_fcm('نأسف جميع الكباتن مشغولين', 'جميع الكباتن مشغولين في رحلات أخرى', $custmoer_fcm);
+            } catch (Exception $e) {
+            }
+            $newPost = $database
+                ->getReference('/customers/' . $trip_request->customer_id)
+                ->update([
+                    'booking_id' => 0,
+                    'booking_status' => 0
+                ]);
 
-    //         $newPost = $database
-    //             ->getReference('/triprequest/' . $trip_request->id)
-    //             ->remove();
-    //     } elseif ($min_driver_id != 0) {
-    //         $fcm = Driver::where('id', $min_driver_id)->value('fcm_token');
-    //         if ($fcm) {
-    //             try {
-    //                 $this->send_fcm('لديك طلب جديد', 'لديك طلب رحلة جديد', $fcm);
-    //             } catch (Exception $e) {
-    //             }
-    //         }
-    //         $newPost = $database
-    //             ->getReference('/drivers/' . $min_driver_id)
-    //             ->update([
-    //                 'booking_status' => 1
-    //             ]);
-    //     } else {
-    //         return false;
-    //     }
-    // }
+            $newPost = $database
+                ->getReference('/triprequest/' . $trip_request->id)
+                ->remove();
+        } elseif ($min_driver_id != 0) {
+            $fcm = Driver::where('id', $min_driver_id)->value('fcm_token');
+            if ($fcm) {
+                try {
+                    $this->send_fcm('لديك طلب جديد', 'لديك طلب رحلة جديد', $fcm);
+                } catch (Exception $e) {
+                }
+            }
+            $newPost = $database
+                ->getReference('/drivers/' . $min_driver_id)
+                ->update([
+                    'booking_status' => 1
+                ]);
+        } else {
+            return false;
+        }
+    }
 
     function distance($lat1, $lon1, $lat2, $lon2, $unit)
     {
