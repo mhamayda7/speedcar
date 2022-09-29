88653f350cc37a2dada55517429996c8353a79f8
Mahamoud Alhamayda <m.hamayda7@gmail.com>
2022-09-29 15:01:26 +0400

    123

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2352,7 +2352,7 @@ class BookingController extends Controller
         $factory = (new Factory)->withServiceAccount(config_path() . '/' . env('FIREBASE_FILE'))
             ->withDatabaseUri(env('FIREBASE_DB'));
         $database = $factory->createDatabase();
-        $newpost = $database->getReference('driver')->push($i);
+        $newpost = $database->getReference('drivers')->push($i);
         // foreach ($drivers as $driver) {
         //     $newPost = $database->getReference('drivers/' . $driver->id )
         //         ->set([
