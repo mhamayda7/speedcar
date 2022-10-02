296a75d9f200e5928e7c281261b7c91052f7aff2
Mahamoud Alhamayda <m.hamayda7@gmail.com>
2022-10-03 00:35:46 +0400

    123

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,12 +27,22 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-
+        // $schedule->command('inspire')->hourly();
+        $schedule->command(command:'TripRequest:timeout')->everyMinute();
+        $schedule->command(command:'changeStatusDriver')->everyMinute();
+        // $schedule->command(command:'ChangeDriverForRequest')->everyMinute();
+        // $schedule->command(command:'command:test')->everyMinute();
+        // $shortSchedule->command(command:'command:test')->everySecond(20);
     }
 
     protected function shortSchedule(ShortSchedule $shortSchedule)
     {
+        $shortSchedule->command('command:test')->everySeconds(30);
+        // $shortSchedule->command(command: 'TripRequest:timeout')->everySeconds(20);
+        // $shortSchedule->command(command: 'changeStatusDriver')->everySeconds(60);
 
+        // // this command will run every half a second
+        // $shortSchedule->command('another-artisan-command')->everySeconds(0.5);
     }
 
     /**
